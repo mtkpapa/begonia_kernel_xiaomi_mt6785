b6482b2b864923ea3c6f236298f3f33399f625a3
mtkpapa <ebanashka10028@gmail.com>
2025-11-21 10:37:46 +0300

    build.sh: Remove LLVM stuff

--- build.sh
+++ build.sh
@@ -11,14 +11,8 @@ MAKE_ARGS=(
     O=out
     "CC=ccache clang"
     "CXX=ccache clang++"
-    AR=llvm-ar
-    NM=llvm-nm
-    OBJDUMP=llvm-objdump
-    STRIP=llvm-strip
     CROSS_COMPILE=aarch64-linux-gnu-
     CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-    LLVM=1
-    LLVM_IAS=1
 )
 
 local_version_str="-perf"
